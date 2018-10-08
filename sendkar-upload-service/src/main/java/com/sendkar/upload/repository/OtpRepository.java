e48b503526cb98ec05f479ebb21fe0f698f20dad
Avishek Das <avishekdas.cool@gmail.com>
2018-10-08 11:20:05 +0530

    upload assisted working

--- sendkar-upload-service/src/main/java/com/sendkar/upload/repository/OtpRepository.java
+++ sendkar-upload-service/src/main/java/com/sendkar/upload/repository/OtpRepository.java
@@ -1,11 +1,9 @@
 package com.sendkar.upload.repository;
 
 import com.sendkar.upload.model.DocumentUploadOtp;
-import com.sendkar.upload.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
